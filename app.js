1acba54b10c48ba0be43bfd27481b6f13e40c0bc
arvindh5686 <aravindh@Aravindhs-MacBook-Air.local>
2015-01-14 21:42:44 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express')
+  , http = require('http')
+
+var app = express()
+
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+  var response = { 
+          automobiles: [ 
+    {"mpg":25, "price":11000, "name":"Subcompact"},
+    {"mpg":16, "price":36400, "name":"Sports"},
+    {"mpg":25, "price":16500, "name":"Compact"},
+    {"mpg":22, "price":72000, "name":"Luxury"}
+  ]
+} 
+  res.header("Access-Control-Allow-Origin", "*");
+  res.send(response);
+});
+
+app.listen(3000);
+
+
